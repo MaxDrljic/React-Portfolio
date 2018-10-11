022d0527e2ee4c6b24314809f8a479d28b2031bf
MaxDrljic <max_spree@hotmail.com>
2018-10-11 10:44:04 +0200

    Landing page content, styles and icons

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+
 
 class Landing extends Component {
   render() {
     return(
-      <div>
-        <h1>Landing Page</h1>
+      <div style={{ width: '100%', margin: 'auto' }}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img 
+              src="https://openclipart.org/image/2400px/svg_to_png/277081/Male-Avatar.png"
+              alt="avatar"
+              className="avatar-img"
+            />
+            <div className="banner-text">
+              <h1>Full Stack Web Developer</h1>
+              <hr />
+
+              <p>HTML/CSS | JavaScript | Angular | React | Vue | NodeJS | PHP | Python</p>
+              <div className="social-links">
+
+                {/* LinkedIn */}
+                <a href="https://google.com" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-linkedin-square" aria-hidden="true" />
+                </a>
+
+                {/* GitHub */}
+                <a href="https://google.com" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-github-square" aria-hidden="true" />
+                </a>
+
+                {/* FreeCodeCamp */}
+                <a href="https://google.com" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-free-code-camp" aria-hidden="true" />
+                </a>
+
+                {/* YouTube */}
+                <a href="https://google.com" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-youtube-square" aria-hidden="true" />
+                </a>
+
+              </div>
+            </div>
+          </Cell>
+        </Grid>
       </div>
     );
   }
