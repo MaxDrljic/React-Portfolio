0730fb6290bd3acbe1e3714841342861f72bdc2e
MaxDrljic <max_spree@hotmail.com>
2018-10-12 11:50:19 +0200

    Projects page

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,108 @@
 import React, { Component } from 'react';
+import {
+   Tabs,
+   Tab, 
+   Grid, 
+   Cell, 
+   Card, 
+   CardTitle,
+   CardText,
+   CardActions, 
+   Button, 
+   CardMenu, 
+   IconButton 
+} from 'react-mdl';
 
 class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: 0
+    }
+  }
+
+  toggleCategories() {
+    if (this.state.activeTab === 0) {
+      return(
+        <div className="projects-grid">
+          {/* Project 1 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: 'black', height: '176px', background: 'url(https://angular.io/assets/images/logos/angular/angular.png) center / 80%'}}>Angular Project #1</CardTitle>
+            <CardText>Lorem ipsum dolor sit amet, pro alia agam erat at, id hinc dictas fierent nec, sea officiis persecuti forensibus an.</CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/* Project 2 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: 'black', height: '176px', background: 'url(https://angular.io/assets/images/logos/angular/angular.png) center / 80%'}}>Angular Project #2</CardTitle>
+            <CardText>Lorem ipsum dolor sit amet, pro alia agam erat at, id hinc dictas fierent nec, sea officiis persecuti forensibus an.</CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/* Project 3 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: 'black', height: '176px', background: 'url(https://angular.io/assets/images/logos/angular/angular.png) center / 80%'}}>Angular Project #3</CardTitle>
+            <CardText>Lorem ipsum dolor sit amet, pro alia agam erat at, id hinc dictas fierent nec, sea officiis persecuti forensibus an.</CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
+      )
+    } else if(this.state.activeTab === 1) {
+      return (
+        <div><h1>This is React</h1></div>
+      )
+    } else if(this.state.activeTab === 2) {
+      return (
+        <div><h1>This is Vue</h1></div>
+      )
+    } else if(this.state.activeTab === 3) {
+      return (
+        <div><h1>This is Laravel</h1></div>
+      )
+    } else if(this.state.activeTab === 4) {
+      return (
+        <div><h1>This is Django</h1></div>
+      )
+    }
+  }
+
   render() {
     return(
-      <div>
-        <h1>Projects Page</h1>
+      <div className="category-tabs">
+        <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+          <Tab>Angular</Tab>
+          <Tab>React</Tab>
+          <Tab>Vue</Tab>
+          <Tab>Laravel</Tab>
+          <Tab>Django</Tab>
+        </Tabs>
+
+        <Grid>
+          <Cell col={12}>
+            <div className="content">{this.toggleCategories()}</div>
+          </Cell>
+        </Grid>      
       </div>
     );
   }
